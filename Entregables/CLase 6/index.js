bead8e765e30cdc91e28eaac4fd8d63134905d99
Enzo Bou <enzo_cba10@hotmail.com>
2022-09-06 23:15:41 -0300

    entrega clase 6

--- Entregables/CLase 6/index.js
+++ Entregables/CLase 6/index.js
@@ -1,4 +1,4 @@
-class Membresia{
+/* class Membresia{
     constructor(nombre, horario, precio, cupos){
         this.nombre = nombre;
         this.horario = horario;
@@ -19,4 +19,23 @@ const membresia2 = new Membresia ("Musculacion", "09:00 a 21:00hs", 2500, 200);
 const membresia3 = new Membresia ("Funcional", "19:00hs", 2000, 20);
 const membresia4 = new Membresia ("Jumping", "18:00hs", 2500, 10);
 membresia1.vender()
-console.log(membresia1);
+console.log(membresia1); */
+
+//APLICANDO ARRAYS AL PROYECTO
+
+const productos = [
+    { id: 1, nombre: "Spinning", precio: 2000, horario: "20:00hs" },
+    { id: 2, nombre: "Musculacion", precio: 2500, horario: "09:00hs a 21:00hs" },
+    { id: 3, nombre: "Funcional", precio: 2000, horario: "19:00hs"},
+    { id: 4, nombre: "Jumping", precio: 2500, horario: "18:00hs" },
+  ];
+
+let nombre = prompt("Ingrese el nombre de la actividad a realizar");
+let producto = productos.find(item => item.nombre === nombre);
+let mensaje = `
+  Id: ${producto.id}
+  Nombre: ${producto.nombre}
+  Precio: ${producto.precio}
+  Horario: ${producto.horario}
+  `;
+alert(mensaje);
