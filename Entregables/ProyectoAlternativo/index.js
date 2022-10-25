31917731a8f35a25a8b7b8e5b0a13d785a8bb463
Enzo Bou <enzo_cba10@hotmail.com>
2022-10-25 19:56:21 -0300

    proyecto final

---
+++ Entregables/ProyectoAlternativo/index.js
@@ -0,0 +1,198 @@
+document.addEventListener('DOMContentLoaded', () => {
+const baseDeDatos = [
+{
+  id: 1,
+  nombre: 'Spinning',
+  precio: 2500,
+  horario: 'Lunes y Martes 20:00hs'
+},
+{
+  id: 2,
+  nombre: 'Musculacion',
+  precio: 2800,
+  horario: 'Lunes a viernes 09:00 a 21:00hs'
+},
+{
+  id: 3,
+  nombre: 'Funcional',
+  precio: 2000,
+  horario: 'Martes y jueves 14:00hs'
+},
+{
+  id: 4,
+  nombre: 'Jumping',
+  precio: 2200,
+  horario: 'Martes y jueves 20:00h'
+},
+{
+  id: 5,
+  nombre: 'Kick Boxing',
+  precio: 2000,
+  horario: 'Miercoles y viernes 19:00hs'
+},
+{
+  id: 6,
+  nombre: 'Spinning',
+  precio: 2200,
+  horario: 'Miercoles y viernes 20:00hs'
+}
+];
+
+let carrito = [];
+const moneda = '$';
+const DOMitems = document.querySelector('#items');
+const DOMcarrito = document.querySelector('#carrito');
+const DOMtotal = document.querySelector('#total');
+const DOMbotonVaciar = document.querySelector('#boton-vaciar');
+const DOMbotonComprar = document.querySelector('#boton-comprar');
+const miLocalStorage = window.localStorage;
+
+function renderizarProductos() {
+  baseDeDatos.forEach((info) => {
+    const miNodo = document.createElement('div');
+    miNodo.classList.add('card', 'col-sm-6');
+    const miNodoCardBody = document.createElement('div');
+    miNodoCardBody.classList.add('card-body');
+    const miNodoTitle = document.createElement('h3');
+    miNodoTitle.classList.add('card-title');
+    miNodoTitle.textContent = info.nombre;
+    const miNodoHorario = document.createElement('h5');
+    miNodoHorario.classList.add('card-text');
+    miNodoHorario.textContent = info.horario;
+    const miNodoPrecio = document.createElement('p');
+    miNodoPrecio.classList.add('card-text');
+    miNodoPrecio.textContent = `${moneda}${info.precio}`;
+    const miNodoBoton = document.createElement('button');
+    miNodoBoton.classList.add('btn', 'btn-primary');
+    miNodoBoton.textContent = '+';
+    miNodoBoton.setAttribute('marcador', info.id);
+    miNodoBoton.addEventListener('click', anyadirProductoAlCarrito);
+    miNodoCardBody.appendChild(miNodoTitle);
+    miNodoCardBody.appendChild(miNodoHorario);
+    miNodoCardBody.appendChild(miNodoPrecio);
+    miNodoCardBody.appendChild(miNodoBoton);
+    miNodo.appendChild(miNodoCardBody);
+    DOMitems.appendChild(miNodo);
+  });
+}
+
+function anyadirProductoAlCarrito(evento) {
+  carrito.push(evento.target.getAttribute('marcador'))
+  renderizarCarrito();
+  guardarCarritoEnLocalStorage();
+}
+
+function renderizarCarrito() {
+  DOMcarrito.textContent = '';
+  const carritoSinDuplicados = [...new Set(carrito)];
+    carritoSinDuplicados.forEach((item) => {
+      const miItem = baseDeDatos.filter((itemBaseDatos) => {
+              return itemBaseDatos.id === parseInt(item);
+      });
+      const numeroUnidadesItem = carrito.reduce((total, itemId) => {
+              return itemId === item ? total += 1 : total;
+      }, 0);
+      const miNodo = document.createElement('li');
+        miNodo.classList.add('list-group-item', 'text-right', 'mx-2');
+        miNodo.textContent = `${numeroUnidadesItem} x ${miItem[0].nombre} - ${miItem[0].precio}${moneda}`;
+      const miBoton = document.createElement('button');
+        miBoton.classList.add('btn', 'btn-danger', 'mx-5');
+        miBoton.textContent = 'X';
+        miBoton.style.marginLeft = '1rem';
+        miBoton.dataset.item = item;
+        miBoton.addEventListener('click', borrarItemCarrito);
+        miNodo.appendChild(miBoton);
+        DOMcarrito.appendChild(miNodo);
+      });
+  DOMtotal.textContent = calcularTotal();
+}
+function borrarItemCarrito(evento) {
+  const id = evento.target.dataset.item;
+  carrito = carrito.filter((carritoId) => {
+    return carritoId !== id;
+  });
+  renderizarCarrito();
+  guardarCarritoEnLocalStorage();
+}
+function calcularTotal() {
+  return carrito.reduce((total, item) => {
+    const miItem = baseDeDatos.filter((itemBaseDatos) => {
+      return itemBaseDatos.id === parseInt(item);
+    });
+      return total + miItem[0].precio;
+  }, 0).toFixed(2);
+}
+function vaciarCarrito() {
+  const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: 'btn btn-success',
+        cancelButton: 'btn btn-danger'
+      },
+      buttonsStyling: false
+    })
+    swalWithBootstrapButtons.fire({
+      title: 'Desea eliminar su carrito?',
+      text: "Este cambio no se puede deshacer",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Si, eliminar carrito',
+      cancelButtonText: 'No, cancelar',
+      reverseButtons: true
+    }).then((result) => {
+      if (result.isConfirmed) {          //             
+        swalWithBootstrapButtons.fire(                                                    
+          'Eliminado!',
+          'Su carrito esta vacio',
+          'success'
+        )
+        carrito = [];
+        renderizarCarrito();
+        localStorage.clear();
+      } else if (
+        result.dismiss === Swal.DismissReason.cancel  //
+      ) {
+        swalWithBootstrapButtons.fire(
+          'Cancelado',
+          'Su carrito sigue lleno',
+          'error'
+        )
+      }
+    })
+}
+function guardarCarritoEnLocalStorage () {
+  miLocalStorage.setItem('carrito', JSON.stringify(carrito));
+}
+
+function cargarCarritoDeLocalStorage () {
+  miLocalStorage.getItem('carrito') !== null && carrito == JSON.parse(miLocalStorage.getItem('carrito'));
+}
+
+function comprarCarrito () {
+  Swal.fire({
+      position: 'center',
+      icon: 'success',
+      title: 'Su compra ha sido realizada con exito',
+      showConfirmButton: false,
+      timer: 1500
+    })
+}
+
+DOMbotonVaciar.addEventListener('click', vaciarCarrito);
+DOMbotonComprar.addEventListener('click', comprarCarrito);
+
+cargarCarritoDeLocalStorage();
+renderizarProductos();
+renderizarCarrito();
+setTimeout(() => {
+  Swal.fire({
+      title: 'Ya conoces nuestras instalaciones?',
+      text: 'Visita nuestra web y encontra tu sucursal mas cercana',
+      imageUrl: 'https://s.yimg.com/ny/api/res/1.2/wJe7xKteba5kncM_Ea1qLA--/YXBwaWQ9aGlnaGxhbmRlcjt3PTk2MDtoPTQ4MTtjZj13ZWJw/https://s.yimg.com/uu/api/res/1.2/4FG.sdU5j_9HdkiZ3lq00w--~B/aD03MjI7dz0xNDQwO2FwcGlkPXl0YWNoeW9u/https://media.zenfs.com/es/runner_s_world_es_761/498b596d34874cf88d174a3cbd099c92',
+      imageWidth: 450,
+      imageHeight: 200,
+      imageAlt: 'Musc',
+    })
+}, 2000)
+});
+
+
--- Entregables/ProyectoFinal/index.js
+++ Entregables/ProyectoFinal/index.js
@@ -1,19 +1,17 @@
- // Variables
- let carrito = [];
- const moneda = '$';
- const DOMitems = document.querySelector('#items');
- const DOMcarrito = document.querySelector('#carrito');
- const DOMtotal = document.querySelector('#total');
- const DOMbotonVaciar = document.querySelector('#boton-vaciar');
- const DOMbotonComprar = document.querySelector('#boton-comprar');
- const miLocalStorage = window.localStorage;
+let carrito = [];
+const moneda = '$';
+const DOMitems = document.querySelector('#items');
+const DOMcarrito = document.querySelector('#carrito');
+const DOMtotal = document.querySelector('#total');
+const DOMbotonVaciar = document.querySelector('#boton-vaciar');
+const DOMbotonComprar = document.querySelector('#boton-comprar');
+const miLocalStorage = window.localStorage;
 
-async function renderProduct() {
-  const response = await fetch("/Entregables/ProyectoFinal/data.json");
-  const data = await response.json();
-  let contenedor = document.getElementById("items");
+fetch("/Entregables/ProyectoFinal/data.json")
+.then((resInicial) => resInicial.json())
+.then((res) => {
 
-  data.forEach((info) => {
+  res.forEach((info) => {
     const miNodo = document.createElement('div');
     miNodo.classList.add('card', 'col-sm-6');
     const miNodoCardBody = document.createElement('div');
@@ -38,133 +36,129 @@ async function renderProduct() {
     miNodoCardBody.appendChild(miNodoBoton);
     miNodo.appendChild(miNodoCardBody);
     DOMitems.appendChild(miNodo);
-});
-};
+})});
+
  
- function anyadirProductoAlCarrito(evento) {
-     carrito.push(evento.target.getAttribute('marcador'))
-     renderizarCarrito();
-     guardarCarritoEnLocalStorage();
- }
+function anyadirProductoAlCarrito(evento) {
+  carrito.push(evento.target.getAttribute('marcador'))
+  renderizarCarrito();
+  guardarCarritoEnLocalStorage();
+}
  
- function renderizarCarrito() {
-     DOMcarrito.textContent = '';
-     const carritoSinDuplicados = [...new Set(carrito)];
-     carritoSinDuplicados.forEach((item) => {
-         const miItem = carrito.filter((itemBaseDatos) => {
-             return itemBaseDatos.id === parseInt(item);
-         });
-         const numeroUnidadesItem = carrito.reduce((total, itemId) => {
-             return itemId === item ? total += 1 : total;
-         }, 0);
-         const miNodo = document.createElement('li');
-         miNodo.classList.add('list-group-item', 'text-right', 'mx-2');
-         miNodo.textContent = `${numeroUnidadesItem} x ${item[0].nombre} - ${item[0].precio}${moneda}`;
-         const miBoton = document.createElement('button');
-         miBoton.classList.add('btn', 'btn-danger', 'mx-5');
-         miBoton.textContent = 'X';
-         miBoton.style.marginLeft = '1rem';
-         miBoton.dataset.item = item;
-         miBoton.addEventListener('click', borrarItemCarrito);
-         miNodo.appendChild(miBoton);
-         DOMcarrito.appendChild(miNodo);
-     });
-     DOMtotal.textContent = calcularTotal();
- }
+function renderizarCarrito() {
+  DOMcarrito.textContent = '';
+  const carritoSinDuplicados = [...new Set(carrito)];
+  carritoSinDuplicados.forEach((item) => {
+    const miItem = carrito.filter((itemBaseDatos) => {
+      return itemBaseDatos.id === parseInt(item);
+    });
+    const numeroUnidadesItem = carrito.reduce((total, itemId) => {
+      return itemId === item ? total += 1 : total;
+    }, 0);
+    const miNodo = document.createElement('li');
+      miNodo.classList.add('list-group-item', 'text-right', 'mx-2');
+      miNodo.textContent = `${numeroUnidadesItem} x ${item[0].nombre} - ${item[0].precio}${moneda}`;
+    const miBoton = document.createElement('button');
+      miBoton.classList.add('btn', 'btn-danger', 'mx-5');
+      miBoton.textContent = 'X';
+      miBoton.style.marginLeft = '1rem';
+      miBoton.dataset.item = item;
+      miBoton.addEventListener('click', borrarItemCarrito);
+      miNodo.appendChild(miBoton);
+      DOMcarrito.appendChild(miNodo);
+  });
+DOMtotal.textContent = calcularTotal();
+}
  
- function borrarItemCarrito(evento) {
-     const id = evento.target.dataset.item;
-     carrito = carrito.filter((carritoId) => {
-         return carritoId !== id;
-     });
-     renderizarCarrito();
-     guardarCarritoEnLocalStorage();
+function borrarItemCarrito(evento) {
+  const id = evento.target.dataset.item;
+  carrito = carrito.filter((carritoId) => {
+    return carritoId !== id;
+  });
+  renderizarCarrito();
+  guardarCarritoEnLocalStorage();
+}
+
+function calcularTotal() {
+  return carrito.reduce((total, item) => {
+    const miItem = res.filter((itemBaseDatos) => {
+      return itemBaseDatos.id === parseInt(item);
+    });
+      return total + miItem[0].precio;
+  }, 0).toFixed(2);
+}
  
- }
+function vaciarCarrito() {
+  const swalWithBootstrapButtons = Swal.mixin({
+    customClass: {
+      confirmButton: 'btn btn-success',
+      cancelButton: 'btn btn-danger'
+    },
+    buttonsStyling: false
+  })
+  swalWithBootstrapButtons.fire({
+    title: 'Desea eliminar su carrito?',
+    text: "Este cambio no se puede deshacer",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonText: 'Si, eliminar carrito',
+    cancelButtonText: 'No, cancelar',
+    reverseButtons: true
+  }).then((result) => {
+    if (result.isConfirmed) {           
+      swalWithBootstrapButtons.fire(                                                    
+        'Eliminado!',
+        'Su carrito esta vacio',
+        'success'
+      )
+      carrito = [];
+      renderizarCarrito();
+      localStorage.clear();
+    } else if (
+      result.dismiss === Swal.DismissReason.cancel
+      ) {
+      swalWithBootstrapButtons.fire(
+        'Cancelado',
+        'Su carrito sigue lleno',
+        'error'
+      )
+    }
+  })
+}
+function guardarCarritoEnLocalStorage () {
+  miLocalStorage.setItem('carrito', JSON.stringify(carrito));
+}
  
- function calcularTotal() {
-     return carrito.reduce((total, item) => {
-         const miItem = data.filter((itemBaseDatos) => {
-             return itemBaseDatos.id === parseInt(item);
-         });
-         return total + miItem[0].precio;
-     }, 0).toFixed(2);
- }
- 
- function vaciarCarrito() {
-         const swalWithBootstrapButtons = Swal.mixin({
-             customClass: {
-               confirmButton: 'btn btn-success',
-               cancelButton: 'btn btn-danger'
-             },
-             buttonsStyling: false
-           })
-           swalWithBootstrapButtons.fire({
-             title: 'Desea eliminar su carrito?',
-             text: "Este cambio no se puede deshacer",
-             icon: 'warning',
-             showCancelButton: true,
-             confirmButtonText: 'Si, eliminar carrito',
-             cancelButtonText: 'No, cancelar',
-             reverseButtons: true
-           }).then((result) => {
-             if (result.isConfirmed) {          //             
-               swalWithBootstrapButtons.fire(                                                    
-                 'Eliminado!',
-                 'Su carrito esta vacio',
-                 'success'
-               )
-               carrito = [];
-               renderizarCarrito();
-               localStorage.clear();
-             } else if (
-               result.dismiss === Swal.DismissReason.cancel  //
-             ) {
-               swalWithBootstrapButtons.fire(
-                 'Cancelado',
-                 'Su carrito sigue lleno',
-                 'error'
-               )
-             }
-           })
- }
- function guardarCarritoEnLocalStorage () {
-     miLocalStorage.setItem('carrito', JSON.stringify(carrito));
- }
- 
- function cargarCarritoDeLocalStorage () {                      
+function cargarCarritoDeLocalStorage () {                      
      /* if (miLocalStorage.getItem('carrito') !== null) {
          carrito = JSON.parse(miLocalStorage.getItem('carrito'));
      } */
-     miLocalStorage.getItem('carrito') !== null && carrito == JSON.parse(miLocalStorage.getItem('carrito'));
- }
+  miLocalStorage.getItem('carrito') !== null && carrito == JSON.parse(miLocalStorage.getItem('carrito'));
+}
  
- function comprarCarrito () {
-     Swal.fire({
-         position: 'center',
-         icon: 'success',
-         title: 'Su compra ha sido realizada con exito',
-         showConfirmButton: false,
-         timer: 1500
-       })
- }
- 
+function comprarCarrito () {
+  Swal.fire({
+    position: 'center',
+    icon: 'success',
+    title: 'Su compra ha sido realizada con exito',
+    showConfirmButton: false,
+    timer: 1500
+  })
+}
 
- DOMbotonVaciar.addEventListener('click', vaciarCarrito);
- DOMbotonComprar.addEventListener('click', comprarCarrito);
- 
- // Inicio
- cargarCarritoDeLocalStorage();
- renderProduct();
- renderizarCarrito();
- setTimeout(() => {
-     Swal.fire({
-         title: 'Ya conoces nuestras instalaciones?',
-         text: 'Visita nuestra web y encontra tu sucursal mas cercana',
-         imageUrl: 'https://s.yimg.com/ny/api/res/1.2/wJe7xKteba5kncM_Ea1qLA--/YXBwaWQ9aGlnaGxhbmRlcjt3PTk2MDtoPTQ4MTtjZj13ZWJw/https://s.yimg.com/uu/api/res/1.2/4FG.sdU5j_9HdkiZ3lq00w--~B/aD03MjI7dz0xNDQwO2FwcGlkPXl0YWNoeW9u/https://media.zenfs.com/es/runner_s_world_es_761/498b596d34874cf88d174a3cbd099c92',
-         imageWidth: 450,
-         imageHeight: 200,
-         imageAlt: 'Musc',
-       })
- }, 2000)
+DOMbotonVaciar.addEventListener('click', vaciarCarrito);
+DOMbotonComprar.addEventListener('click', comprarCarrito);
+
+cargarCarritoDeLocalStorage();
+renderizarCarrito();
+setTimeout(() => {
+  Swal.fire({
+    title: 'Ya conoces nuestras instalaciones?',
+    text: 'Visita nuestra web y encontra tu sucursal mas cercana',
+    imageUrl: 'https://s.yimg.com/ny/api/res/1.2/wJe7xKteba5kncM_Ea1qLA--/YXBwaWQ9aGlnaGxhbmRlcjt3PTk2MDtoPTQ4MTtjZj13ZWJw/https://s.yimg.com/uu/api/res/1.2/4FG.sdU5j_9HdkiZ3lq00w--~B/aD03MjI7dz0xNDQwO2FwcGlkPXl0YWNoeW9u/https://media.zenfs.com/es/runner_s_world_es_761/498b596d34874cf88d174a3cbd099c92',
+    imageWidth: 450,
+    imageHeight: 200,
+    imageAlt: 'Musc',
+  })
+}, 2000)
 
